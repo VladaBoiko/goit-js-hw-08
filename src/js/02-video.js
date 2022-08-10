b11972e264c86418c86a967c22363fd3d03473e6
VladaBoiko <bermezha@gmail.com>
2022-08-10 19:38:06 +0300

    add second

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,12 @@
-// import throttle from 'lodash.throttle';
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const VIDEO_DURATION = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(VIDEO_DURATION, data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem(VIDEO_DURATION));
